280daf68900b469f2da5286919f007c2b60f7cd1
Vaibhav Gurav <151390343+VaibhavGurav07@users.noreply.github.com>
2024-11-29 15:55:39 +0000

    class base view

--- petstore/petapp/urls.py
+++ petstore/petapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from petapp.views import MyView
 
 urlpatterns = [
     path("home/",views.home),
+    path('about',MyView.as_view())
 ]
--- petstore/petapp/views.py
+++ petstore/petapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from django.views import View
 
 # Create your views here.
 
 def home(request):
     return HttpResponse('hello is home page using def function base ')
+
+class MyView(View):
+
+    def get(self,request):
+
+        return HttpResponse('hello from class base view ')
